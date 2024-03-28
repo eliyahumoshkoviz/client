470508fdb51fede2070bae567db2fb91dbbda46f
eliyahumoshkoviz <054epm@gmail.com>
2024-03-28 15:25:25 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,15 @@ import React from "react";
 import Badge from "./componets/Badge";
 import Send from "./componets/Send";
 import Trash from "./componets/Trash";
+import EmailLi from "./componets/EmailLi/insex";
 
 export default function App() {
   return (
     <div>
-      {/* <div style={{ display: "flex" }}>
-        <img src="./vite.svg" alt="" />
-        <div>
-          <h3>Eli mosh</h3>
-          <p>hey Eli ...</p>
-        </div>
-        <div>
-          <p>11:30</p>
-          <Badge>12</Badge>
-        </div>
-      </div> */}
-      <Send></Send>
-      <Trash/>
+      <EmailLi/>
+    
+      {/* <Send></Send>
+      <Trash/> */}
     </div>
   );
 }
